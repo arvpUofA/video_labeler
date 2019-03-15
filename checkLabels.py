ef2bc9b597a083fe42e0e930c4143eefa8c5f0d2
Justin Francis <jkf@ualberta.ca>
2019-03-15 13:52:33 -0600

    combine labels fix (#11)
    * combine labels fix
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * fixed issue #8
    
    * fixed spelling mistake
    
    * fixed video labeler and checkLabel bugs
    
    * slightly better variable names
    
    * more fixes
    
    * small comment on readme

--- checkLabels.py
+++ checkLabels.py
@@ -1,37 +1,43 @@
 import numpy as np 
 import cv2
 import os, sys
-from pathlib import Path
 
 class LabelChecker:
-    def __init__(self):
-        self.rootPath = Path.cwd()
-        self.directories = ['badData', 'goodData']
-        self.dirDict = {}
-        for i in self.directories:
-            self.dirDict[i] = self.rootPath / i
+    def __init__(self, image_directory_name):
+        self.cwd = os.getcwd()
+        self.image_directory_name = image_directory_name
 
-
-    def getImageFileList(self, path):
+    def getImageFileList(self, directory_name):
         '''RETURNS A LIST OF ALL FILES IN /DATA DIR THAT DO NOT END IN .TXT'''
-        imageFileList = os.listdir(self.dirDict[path].as_posix())
-        imageFileList = [i for i in imageFileList if i[-3:] != 'txt']
-        return imageFileList
+        image_file_list = os.listdir(os.path.join(self.cwd, directory_name))
+        image_file_list = [i for i in image_file_list if not i.endswith('txt')]
+        return image_file_list
     
-    def loadImage(self, fileName, path): 
-        '''TAKES IN IMAGE FILENAME AND RETURNS IMAGE, OBJECT SIZE (HxW), 
+    def loadImage(self, file_name, directory_name): 
+        '''TAKES IN IMAGE file_name AND RETURNS IMAGE, OBJECT SIZE (HxW), 
         CLASSES AND LABELS'''
-        image = cv2.imread((self.dirDict[path] / fileName).as_posix())
+        directory = os.path.join(self.cwd, directory_name)
+        image = cv2.imread(os.path.join(directory, file_name))
         sizeHW = [image.shape[0], image.shape[1]]
-        textFile = fileName.split('.')[0]+'.txt'
-        labels = (self.dirDict[path] / textFile).open()
-        labels = [i.strip() for i in labels]
-        labels = [i.split() for i in labels]
-        labels = np.asarray(labels)
-        classes = labels[:,0].astype(np.int)
-        labels = labels[:,1:].astype(np.float)
-        return image, sizeHW, classes, labels
-    
+        text_file = file_name.split('.')[0]+'.txt'
+        
+        hasLabel = True
+        try:
+            label_file = open(os.path.join(directory, text_file))
+        except:
+            hasLabel = False
+        
+        if hasLabel:    
+            labels = [i.strip() for i in label_file]
+            labels = [i.split() for i in labels]
+            labels = np.asarray(labels)
+            classes = labels[:,0].astype(np.int)
+            labels = labels[:,1:].astype(np.float)
+            label_file.close()
+            return image, sizeHW, classes, labels
+        else:
+            return image, sizeHW, [], []
+
     def drawBox(self, img, sizeHW, classes, labels, window):
         '''TAKES IN LABELS IN YOLO FORMAT AND CREATES ANNOTATED IMAGE'''
         font = cv2.FONT_HERSHEY_SIMPLEX
@@ -52,53 +58,58 @@ class LabelChecker:
         else:
             return img
     
-    def checkLabels(self, path):
+    def checkLabels(self, directory_name):
         '''LOOPS THROUGH DATA IN YOUR DATA FOLDER GIVING A PREVIEW OF LABELED
-        IMAGE ALLOWING YOU TO REMOVE IMPROPERLY LABELED IMAGES IF MOVE IS TRUE
-        IT WILL MOVE CHECKED DATA INTO SORTED FOLDERS IN ROOT DIRECTORY'''
-        print('Press ESC to quit')
-        filelist = self.getImageFileList(path)
-        move = input('Do you want to seperate sorted data? (Y/N): ').lower()     
-        if move == 'y':
-            try:
-                self.dirDict['goodData'].mkdir()
-            except Exception as e:
-                print(e.args[0])
-            try:
-                self.dirDict['badData'].mkdir()
-            except Exception as e:
-                print(e.args[0])
-            print('Press Spacebar to move image into badData dir')
-            print('Press any other key to keep data')
-        
-        for fileName in filelist:
-            img, sizeHW, classes, labels, = self.loadImage(fileName, path)
-            key,img = self.drawBox(img, sizeHW, classes, labels, window=True)
-            image = self.dirDict[path] / fileName
-            label = self.dirDict[path] / (fileName.split('.')[0]+'.txt')
+        IMAGE ALLOWING YOU TO REMOVE IMPROPERLY LABELED IMAGES'''
+        print('RUNNING THIS WILL MODIFY YOUR {images} folder(in same directory of this script)')
+        file_list = self.getImageFileList(directory_name)
+        option = -1
+        while option not in [0, 1]:
+            option = int(input('0:remove images and labels with space, any other key to continue \n'
+                            '1:genrate train.txt:\n'
+                            '[0/1]: '))    
+        if option == 0:
+            print('Press \'x\' to remove image, any other key to continue, ESC to quit')
+        elif option == 1:
+            all_label_file = open(os.getcwd() + '/train.txt','w')
+
+        file_list.sort();
+        directory = os.path.join(self.cwd, directory_name)
+        for file_name in file_list:
+            if option == 1:
+                label_string = 'arvpData/images/train/'+file_name+'\n'
+                all_label_file.write(label_string)
+                continue
+
+            img, sizeHW, classes, labels, = self.loadImage(file_name, directory_name)
+            image_path = os.path.join(directory, file_name)
+            print(image_path)
             
-            if key == 32 and move == 'y':
-                print('Removing', fileName)
-                image.replace(self.dirDict['badData'] / image.name)
-                label.replace(self.dirDict['badData'] / label.name)
+            key,img = self.drawBox(img, sizeHW, classes, labels, window=True)
+
+            # label file to remove
+            label_file = os.path.join(directory, file_name.split('.')[0]+'.txt')
+            if (option == 0) and key == ord('x'): 
+                if os.path.isfile(image_path) and os.path.isfile(label_file):
+                    print(image_path + " has been removed")
+                    os.remove(image_path)
+                    os.remove(label_file)
+                else:
+                    print image_path + "not found"
             elif key == 27:
-                break
-            elif move =='y':
-                image.replace(self.dirDict['goodData'] / image.name)
-                label.replace(self.dirDict['goodData'] / label.name)
+                break                        
+
         cv2.destroyAllWindows() 
-        
+        if option == 1:            
+            all_label_file.close()
                  
 def main():
-    labelChecker = LabelChecker()
-
-    directoryName = 'images'    
     if len(sys.argv) > 1:
-        directoryName = sys.argv[1]
+        image_directory_name = sys.argv[1]
+    else:
+        print("Usage python checkLabels.py {image_with_labels directoy name}\nMust be in same directory as this script")
+        exit()
+    labelChecker = LabelChecker(image_directory_name)
+    labelChecker.checkLabels(directory_name=image_directory_name)    
 
-    labelChecker.dirDict[directoryName] = labelChecker.rootPath / directoryName
-
-    if labelChecker.dirDict[directoryName].is_dir():
-        labelChecker.checkLabels(path=directoryName)
-            
 main()
--- combineLabels.py
+++ combineLabels.py
@@ -1,59 +1,86 @@
-import os
+import os, sys
 
 ''' ####### INSTRUCTIONS #######
-- this script assumes the image files are sorted via their name
-- place images to label in folder called images
-- begin create labels files starting with 0.txt and incrementing by one
-  for eah new label
-- run python combineLabels.py
-- rename images folder, delete 0-[num_classes].txt from root dir
-- begin again
-- to sanity check data move into data folder in rotation_augment script
-  root folder: https://github.com/wagonhelm/rotation_augment
+Usage: python2 <image directory name> <num of classes>
+Output: - will not create any labels if bounding box = [0,0,0,0]
+        - creates individual train files for each image
+
 '''
 
 cwd = os.getcwd()
-images = os.listdir(cwd+'/images')
-images = [i.split('.')[0] for i in images]
-images = sorted(images)
-# X width, Y height
-img_size = [1920,1080]
-num_classes = 6
 
-def convert_2_yolo(coords):
-    # Yolo labels are [center x, center y, width height] w.r.t to image size
-    x_center = coords[0] + coords[2]/2
-    y_center = coords[1] + coords[3]/2
-    coords[0] = x_center / img_size[0]
-    coords[1] = y_center / img_size[1]
-    coords[2] = coords[2] / img_size[0]
-    coords[3] = coords[3] / img_size[1]
+if len(sys.argv) == 3:
+    directory_name = sys.argv[1]
+    num_classes = int(sys.argv[2])
+else:
+    print("Usage python {image directory name} {num of classes}")
+    exit()
 
-# Labels are in Top Left X, Top Left Y, Box Width, Box Height
+choice = ''
+# Remove pre existing labels
+for files in os.listdir(os.path.join(cwd, directory_name)):
+    if files.endswith(".txt"):
+        while choice.lower() not in ['n', 'y']:
+            choice = raw_input("Removing pre-exisitng combined labels?[y/n] ").lower()
+
+            if choice.lower() == 'y':
+                for files2 in os.listdir(os.path.join(cwd, directory_name)):
+                    if files2.endswith(".txt"):
+                        os.remove(os.path.join(cwd, directory_name, files2))
+                break
+            elif choice.lower() == 'n':    
+                print("Script will not work with pre-existing labels")
+                exit()
+        
+image_names = os.listdir(os.path.join(cwd, directory_name))
+image_names = [i for i in image_names]
+image_names = sorted(image_names)
+# Labels are in YOLO format
 labels = [[None] for i in range(num_classes)]
 
 for i in range(num_classes):
-    cur_class = open(str(i)+'.txt','r')
-    label = [i.strip() for i in cur_class]
-    label = [i.split(' ') for i in label]
-    for line in label:
-        for ii in range(4):
-            line[ii] = int(line[ii])
-    labels[i] = label
-    cur_class.close()
-     
-for i in labels:
-    for line in i:
-        line = convert_2_yolo(line)
+    class_label_file = open(str(i)+'.txt','r')
 
-for i in range(len(images)):
-    label = open(cwd+'/images/'+images[i]+'.txt','w')
+    # label is a list of
+    # ['0', '0', '0', '0', 'pathGateDice/00812.jpg']
+    class_labels = [l.strip().split(' ') for l in class_label_file]
+    for label in class_labels:
+        for j in range(4):
+            label[j] = float(label[j])
+
+    # Assert the file still exists.
+    # File not existing should never be possible, but in case it is
+    to_remove = []
+    for label in class_labels:
+        if (not os.path.isfile(os.path.join(cwd, label[-1]))):
+            print("could not find" + label[-1] + " did you remove it?")
+            to_remove.append(label)
+    for removed in to_remove:
+        class_labels.remove(removed)
+    
+    labels[i] = class_labels
+    class_label_file.close()
+
+for i in range(len(image_names)):
+    image_name = image_names[i]
+    label_strings = []
     for class_num in range(num_classes):
-        string = "{} {:.6f} {:.6f} {:.6f} {:.6f}".format(
-                  class_num,
-                  labels[class_num][i][0],
-                  labels[class_num][i][1],
-                  labels[class_num][i][2],
-                  labels[class_num][i][3])
-        label.write(string+'\n')
-    label.close()
+        total = sum(labels[class_num][i][0:3])
+        if total != 0.0:
+            label_string = "{} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(
+                    class_num,
+                    labels[class_num][i][0],
+                    labels[class_num][i][1],
+                    labels[class_num][i][2],
+                    labels[class_num][i][3])
+            label_strings.append(label_string)
+    # remove the image if there are no labels
+    if len(label_strings) == 0:
+        print(image_name + ' has no label and is removed')
+        os.remove(os.path.join(cwd, directory_name, image_name))
+    else:
+        label_file = open(os.path.join(cwd, directory_name, image_name.split('.')[0] + '.txt'),'w')
+        for l in label_strings:
+            label_file.write(l)
+        label_file.close()
+
